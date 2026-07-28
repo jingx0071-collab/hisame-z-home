437655cc8a196a48d2a0771a632c24bcb5968a96
Hisame <hisame.z.home@gmail.com>
2026-07-28 11:02:52 -0700

    appbar v13: header takes a slot in flow instead of floating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,11 +58,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider><RealtimeProvider><div className="app-shell">{children}</div></RealtimeProvider></ThemeProvider>
+        <ThemeProvider><RealtimeProvider><div className="app-shell"><AppHeader />{children}</div></RealtimeProvider></ThemeProvider>
         <PushRegistrar />
         <PushButton />
         <ViewportFix />
-        <AppHeader />
       </body>
     </html>
   );
